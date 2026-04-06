a89ee0deefbb6338b95d755526f066ca035fe67d
Markus <markus@1e59-f9f7-58e7-8a00-0180-84b7-07d0-2001.dyn.estpak.ee>
2026-04-06 19:45:07 +0300

    Now if a hex has building or units on it, they will also be loaded as an overlay on that hex

--- script.js
+++ script.js
@@ -13,24 +13,53 @@ async function getMap(){
     console.log(hex)
     for (const item of hex) {
         if (item.type !== "impassable"){
-        
+            
+            let imageContainer = document.createElement('div');
             let image = document.createElement('img');
             image.src = `https://tinkr.tech/${item.image}`
-           
-            mapContainer.appendChild(image)
-
-            image.classList.add('mapHex')
-            image.style.left = item.x + 'px';
-            image.style.top = item.y +'px';
-            image.style.width = item.width + 'px';
-            image.style.height =item.height + 'px';
             
+            mapContainer.appendChild(imageContainer);
+            imageContainer.appendChild(image);
+
+            mapContainer.classList.add('mapHexContainer');
+
+            drawOnHex(image, item);
+            
+        if (item.building !== null) {
+            let buildingImg = document.createElement('img');
+            buildingImg.src = `https://tinkr.tech/${item.building_image}`;
+
+            imageContainer.appendChild(buildingImg);
+
+            drawOnHex(buildingImg,item);
+        }
+        if (item.unit !== null) {
+            let unitImg = document.createElement('img');
+            unitImg.src = `https://tinkr.tech/${item.unit_image}`
+
+            imageContainer.appendChild(unitImg);
+
+            drawOnHex(unitImg, item);
+        }
+
         
-            console.log(item);}
+            console.log(item);
+        };
+        
+        
     }
 } 
 
     
+function drawOnHex(obj, item){
+    obj.classList.add('mapHex')
+    obj.style.left = item.x + 'px';
+    obj.style.top = item.y +'px';
+    obj.style.width = item.width + 'px';
+    obj.style.height =item.height + 'px';
+}
+     
+
 
 getMap();
 
