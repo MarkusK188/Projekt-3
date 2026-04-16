5baa095081a400da015155b8f605bd9af3050ea6
Markus <markus@1e59-f9f7-58e7-8a00-0180-84b7-07d0-2001.dyn.estpak.ee>
2026-04-16 15:40:31 +0300

    Add sort of functionality to join and start game

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const mapContainer = document.querySelector('#mapContainer');
 const joinBtn = document.querySelector('#joinBtn');
+const userInput = document.querySelector('#userInput');
+const startGameBtn = document.querySelector('#startGameBtn');
 
 let maps;
 
+let playerKey;
 
 
+const url = 'https://tinkr.tech/sdb/markus_namespace/antiyoy1';
 
 async function getMap(){
-    const response = await fetch('https://tinkr.tech/sdb/markus_namespace/antiyoy1');
+    const response = await fetch(url);
     const data = await response.json();
     let hex = data.map;
 
@@ -64,5 +68,59 @@ function drawOnHex(obj, item){
 setInterval(function() {
     getMap();
     console.log('refreshed');
+    let playerKey = localStorage.getItem("player_key");
+      console.log(playerKey);
+
 }, 2000);
 
+joinBtn.addEventListener('click', function(){
+    if (userInput.value.trim() !== "" ){
+        let name = userInput.value.trim();
+        joinGame(name);}
+    else {
+        console.log("You must enter a username to join!")}
+        console.log(userInput.value.trim() );
+});
+
+startGameBtn.addEventListener('click', function(){
+    startGame();
+});
+
+async function joinGame(playerName) {
+        const join = {
+             action: "join",
+            username: playerName
+            };
+    const response = await fetch(url, {
+     method: 'POST',
+     headers: {
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(join)
+  });
+  
+  const result = await response.json();
+  let playerKey = result.player_key;
+
+   localStorage.setItem("player_key", playerKey);       
+  console.log(result);
+  console.log(playerKey);
+    
+};
+
+async function startGame(){
+    const start = {
+        action: "start"
+    }
+
+    const respone = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(start)
+    });
+    const result = await respone.json();
+    console.log(result);
+    
+};
